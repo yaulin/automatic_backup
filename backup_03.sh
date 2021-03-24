077300cdd05968704813dffcdd1fee03f578a584
Yaroslav Aulin <yaroslav.v.aulin@gmail.com>
2021-03-24 18:12:31 +0000

    Add files via upload

---
+++ backup_03.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+date >> ~/shell_scripts/output.txt
+echo "backup started" >> ~/shell_scripts/output.txt
+timestamp=$(date +%Y-%m-%d-%H:%M)
+mkdir "/home/yaroslav/Backup/backup_$timestamp"
+rsync -a ~/Work/ "/home/yaroslav/Backup/backup_$timestamp"
+date >> ~/shell_scripts/output.txt
+echo "backup successful" >> ~/shell_scripts/output.txt 
+
